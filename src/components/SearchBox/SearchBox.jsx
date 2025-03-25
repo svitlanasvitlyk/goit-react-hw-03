7ec80661d556dd0e1a137b30caa561ee504e692d
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2025-03-25 15:33:36 +0100

    update searchbox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,6 +3,7 @@ import s from "./SearchBox.module.css";
 const SearchBox = ({ filter, onFilter }) => {
   return (
     <div className={s.container}>
+      <p>Find contacts by name</p>
       <input
         value={filter}
         onChange={(e) => onFilter(e.target.value)}
